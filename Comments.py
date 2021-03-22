3e4fae7fdda09ce7fc584afedd5a1414200a40c6
Bishajit <67888547+Bishajit@users.noreply.github.com>
2021-03-21 21:02:59 -0400

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,7 @@
+# Print Ocean is blue
+print ("Ocean is blue")
+
+# Calculates and returns the square of a number
+#prvios comment is bad because too long
+def square(number):
+    return number * number
